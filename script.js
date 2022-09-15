91584d3892e39dda1e336436f2759d657160ded0
Theophilus Adesola <theophilusadessola@gmail.com>
2022-09-15 13:17:29 +0100

    stuck

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const startBtnDiv = document.getElementById("start-btn-div")
 const startBtn = document.getElementById("start-btn")
+const ruleDiv = document.getElementById("rules-div")
+const agreeBtn = document.getElementById("agree-btn")
+const quizDiv = document.getElementById("quiz-div")
+
+startBtn.addEventListener("click", ()=>{
+    startBtn.classList.add("hide")
+    ruleDiv.classList.remove("hide")
+})
+ agreeBtn.addEventListener("click", ()=>{
+    ruleDiv.classList.add("hide")
+    quizDiv.classList.remove("hide")
+ })
+
+//  const selectAns = () =>{
+//     let options = document.querySelectorAll("option")
+    
+//  }
+let options = document.querySelectorAll("option-btn")
+(options.forEach(()=>{
+    console.log(option)
+}))
+
