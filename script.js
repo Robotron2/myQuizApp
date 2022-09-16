d272b71d277f52970d4eb827aba5f1d88a045023
Theophilus Adesola <theophilusadessola@gmail.com>
2022-09-16 10:50:37 +0100

    Answer options are being displayed correctly

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const startBtn = document.getElementById("start-btn")
 const ruleDiv = document.getElementById("rules-div")
 const agreeBtn = document.getElementById("agree-btn")
 const quizDiv = document.getElementById("quiz-div")
-let options = document.querySelectorAll(".option-btn")
 let questionElement = document.getElementById("question")
+let optionOne = document.getElementById("option-one")
+let optionTwo = document.getElementById("option-two")
+let optionThree = document.getElementById("option-three")
+let optionFour = document.getElementById("option-four")
 let shuffledQuestion, currentQuestionIndex
 
 
@@ -30,13 +33,15 @@ const setQuestion = () =>{
 
 const showQuestion = (param) =>{
     questionElement.innerText = param.question
+    console.log(param.answers)
+    optionOne.innerHTML = param.answers[0].ans
+    optionTwo.innerHTML = param.answers[1].ans
+    optionThree.innerHTML = param.answers[2].ans
+    optionFour.innerHTML = param.answers[3].ans
 }
 
 
+const selectOption = () =>{
 
-
-
-
-
-
+}
 
