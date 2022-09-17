b3f0ea1fb701cdc9723a2fb592fe5da47d67b7d1
Theophilus Adesola <theophilusadessola@gmail.com>
2022-09-17 15:48:50 +0100

    Feature to disable all buttons from clicking after the last question

--- script.js
+++ script.js
@@ -9,15 +9,44 @@ let optionOne = document.getElementById("option-one");
 let optionTwo = document.getElementById("option-two");
 let optionThree = document.getElementById("option-three");
 let optionFour = document.getElementById("option-four");
+let modal = document.getElementById("resultModal");
+let scoredPoints = 0;
 let shuffledQuestion, currentQuestionIndex;
 shuffledQuestion = questions.sort(() => Math.random() - 0.5);
 currentQuestionIndex = 0;
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
 
 const setQuestion = () => {
     if (currentQuestionIndex < questions.length) {
         showQuestion(shuffledQuestion[currentQuestionIndex]);
     } else {
         console.log("No more questions");
+        modal.style.display = "block";
+        for (let index = 0; index < optionBtnGroup.length; index++) {
+            optionBtnGroup[index].classList.add("disable");
+        }
     }
 };
 
@@ -62,10 +91,15 @@ const checkAns = (clickedOption) => {
         console.log("Correct answer");
         console.log(`You clicked this ${clickedOption}`);
         console.log(`This is the correct answer ${shuffledQuestion[currentQuestionIndex].correctAns}`);
+        scoredPoints += 10;
     } else {
         console.log("wrong");
     }
 };
+
+// const showResult = () =>{
+
+// }
 
 // const selectOption = () =>{}
 
