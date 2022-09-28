77a06fa5fea171fc38d7348ab39c1eca2e8f1fbc
Adeleke Theophilus <theophilusadesola002@gmail.com>
2022-09-28 22:25:34 +0100

    close btn reloads the page

--- script.js
+++ script.js
@@ -49,10 +49,11 @@ var span = document.getElementsByClassName("close")[0];
 
 exitBtn.addEventListener("click", () => {
     modal.style.display = "none";
+    window.location.href = "index.html"
 });
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
+window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
